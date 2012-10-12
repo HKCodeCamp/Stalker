a479ae74f58539bb929d57abb04be87b724709f7
Copland Ng <copland.ng@gmail.com>
2012-10-12 23:02:45 +0800

    added preliminary lungoJS UI

---
+++ hybrid/assets/www/app/app.js
@@ -0,0 +1,13 @@
+var App = (function(lng, undefined) {
+
+    //Define your LungoJS Application Instance
+    lng.App.init({
+        name: 'Stalker',
+        version: '0.1'
+    });
+
+    return {
+
+    };
+
+})(LUNGO);
