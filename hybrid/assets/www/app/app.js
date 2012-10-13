476e178c00c5750f498d43f06e7db7c207ae82bd
Copland Ng <copland.ng@gmail.com>
2012-10-13 09:44:31 +0800

    adding avatars images and splash screen logo

--- hybrid/assets/www/app/app.js
+++ hybrid/assets/www/app/app.js
@@ -1,13 +1,17 @@
 var App = (function(lng, undefined) {
 
-    //Define your LungoJS Application Instance
-    lng.App.init({
-        name: 'Stalker',
-        version: '0.1'
-    });
+	// Define your LungoJS Application Instance
+	lng.App.init({
+		name : 'Stalker',
+		version : '0.1',
+		sugars : [ 'map', 'pull' ],
+		resources : {
+			sections : []
+		}
+	});
 
-    return {
+	return {
 
-    };
+	};
 
 })(LUNGO);
