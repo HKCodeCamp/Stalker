023718fa909ee5087721810ef907f1db9cdfda30
Copland Ng <copland.ng@gmail.com>
2012-10-13 16:49:43 +0800

    added list news

--- hybrid/assets/www/app/events.js
+++ hybrid/assets/www/app/events.js
@@ -1,13 +1,10 @@
 App.Events = (function(lng, app, undefined) {
 
 	lng.ready(function() {
-		// App.Services.mockProfiles();
-		// lng.View.Aside.show('#kitchen-sink', '#kitchen-sink-scroll');
-		// lng.View.Element.progress('.progress', 10, true, 'Downloading
-		// 1/5...');
+		App.Services.listNews();
 	});
 
-	lng.dom('article#celebrity-list a').tap(function(event) {
+	lng.dom('article#celebrity a').tap(function(event) {
 		App.Services.searchCelebrities($$('article#celebrity-list input').val());
 	});
 
--- hybrid/assets/www/app/services.js
+++ hybrid/assets/www/app/services.js
@@ -1,22 +1,37 @@
 App.Services = (function(lng, app, undefined) {
 
 	lng.Service.Settings = {
-			async : true,
-			timeout : 1000
-		};
+		async : true,
+		timeout : 1000
+	};
 
 	var root = 'http://istalkerapp.appspot.com/'
-		
-		
-	var searchCelebrities = function(search) {
-//		var url = root + 'search/' + search + '/';
+
+	var listNews = function() {
 		var url = root + 'list/';
-		
 		var data = {};
+
+		lng.Service.json(url, data, function(response) {
+			for (i in response) {
+				response[i]['image'] = i;
+				var parameters = {
+					el : '#news',
+					template : 'news-item',
+					data : response[i]
+				};
+				lng.View.Template.List.create(parameters);
+			}
+		});
+	}
+
+	var searchCelebrities = function(search) {
+		var url = root + 'search/' + search + '/';
+		var data = {};
+
 		lng.Service.json(url, data, function(response) {
 			for (i in response) {
 				var parameters = {
-					el : '#celebrity-list',
+					el : '#celebrity',
 					template : 'celebrity-item',
 					data : response[i]
 				};
@@ -26,6 +41,7 @@ App.Services = (function(lng, app, undefined) {
 	}
 
 	return {
+		listNews : listNews,
 		searchCelebrities : searchCelebrities
 	}
 
